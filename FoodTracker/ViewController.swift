2efb0d4615c8d051056e71129b8305d8d5fb2fde
Ken Wilcox <wilcoxk@gmail.com>
2015-02-21 21:35:19 -0700

    conforming to needed protocols

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class ViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
+  
+  var searchController: UISearchController!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -21,6 +24,38 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+}
+
+// MARK: UITableViewDelegate
+extension ViewController: UITableViewDelegate {
   
 }
 
+// MARK: UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    var cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 0
+  }
+}
+
+// MARK: UISearchBarDelegate
+extension ViewController: UISearchBarDelegate {
+  
+}
+
+// MARK: UISearchControllerDelegate
+extension ViewController: UISearchControllerDelegate {
+  
+}
+
+// MARK: UISearchResultsUpdating
+extension ViewController: UISearchResultsUpdating {
+  func updateSearchResultsForSearchController(searchController: UISearchController) {
+    
+  }
+}
