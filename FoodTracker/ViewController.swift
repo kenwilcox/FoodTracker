035ef509d6639f0eef71e00305f30b8b8a1d23d1
Ken Wilcox <wilcoxk@gmail.com>
2015-03-01 23:18:18 -0700

    using POST instead of GET

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -107,11 +107,41 @@ extension ViewController: UISearchResultsUpdating {
   }
   
   func makeRequest(searchString: String) {
-    let url = NSURL(string: "https://api.nutritionix.com/v1_1/search/\(searchString)?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=\(kAppId)&appKey=\(kAppKey)")
-    let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+    // Get Request - not using this, but kept for reference.
+//    let url = NSURL(string: "https://api.nutritionix.com/v1_1/search/\(searchString)?results=0%3A20&cal_min=0&cal_max=50000&fields=item_name%2Cbrand_name%2Citem_id%2Cbrand_id&appId=\(kAppId)&appKey=\(kAppKey)")
+//    let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+//      var stringData = NSString(data: data, encoding: NSUTF8StringEncoding)
+//      println(stringData)
+//      println(response)
+//    })
+//    task.resume()
+    
+    var request = NSMutableURLRequest(URL: NSURL(string: "https://api.nutritionix.com/v1_1/search/")!)
+    let session = NSURLSession.sharedSession()
+    request.HTTPMethod = "POST"
+    
+    var params = [
+      "appId" : kAppId,
+      "appKey" : kAppKey,
+      "fields" : ["item_name", "brand_name", "keywords", "usda_fields"],
+      "limit"  : "50",
+      "query"  : searchString,
+      "filters": ["exists":["usda_fields": true]]
+    ]
+    
+    var error: NSError?
+    request.HTTPBody = NSJSONSerialization.dataWithJSONObject(params, options: nil, error: &error)
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    
+    var task = session.dataTaskWithRequest(request, completionHandler: { (data, response, err) -> Void in
       var stringData = NSString(data: data, encoding: NSUTF8StringEncoding)
       println(stringData)
-      println(response)
+      
+      var conversionError: NSError?
+      var jsonDictionary = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableLeaves, error: &conversionError) as? NSDictionary
+      
+      println(jsonDictionary)
     })
     task.resume()
   }
