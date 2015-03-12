5229e48a62e58241ba425f406ab7906dbbedf821
Ken Wilcox <wilcoxk@gmail.com>
2015-03-11 22:56:42 -0600

    Added HealthStoreConstant

---
+++ FoodTracker/HealthStoreConstant.swift
@@ -0,0 +1,14 @@
+//
+//  HealthKitConstant.swift
+//  FoodTracker
+//
+//  Created by Kenneth Wilcox on 3/11/15.
+//  Copyright (c) 2015 Kenneth Wilcox. All rights reserved.
+//
+
+import Foundation
+import HealthKit
+
+class HealthStoreConstant {
+  let healthStore: HKHealthStore? = HKHealthStore()
+}
